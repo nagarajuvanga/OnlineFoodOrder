e9862e29428913f3062ba0be6647ffe86d3dfbde
AndroidNaveen <naveenandroid6557@gmail.com>
2020-09-04 10:44:33 +0530

    Admin Menu Options
    In this commit we are done upto admin menu options desing and page opening
    
    Menu Operations are pending

--- OnlineFood/pwn/urls.py
+++ OnlineFood/pwn/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('',views.showIndex,name='pwn_main'),
     path('pwn_login_check/',views.pwn_login_check,name='pwn_login_check'),
     path('welcome/',views.welcome,name='welcome'),
+    path('state/',views.openState,name='state'),
+    path('city/',views.openCity,name='city'),
+    path('cuisine/',views.openCusine,name='cuisine'),
+    path('vendor/',views.openVendor,name='vendor'),
+    path('resports/',views.openReporsts,name='reports'),
+    path('logout/',views.pwn_login_check,name='logout'),
 ]
--- OnlineFood/pwn/views.py
+++ OnlineFood/pwn/views.py
@@ -6,13 +6,39 @@ def showIndex(request):
 
 
 def pwn_login_check(request):
-    try:
-        admin = AdminLoginModel.objects.get(username=request.POST.get("pwn_username"),password=request.POST.get("pwn_password"))
-        request.session["admin_status"] = True
-        return redirect('welcome')
-    except:
-        return render(request,"pwn/login.html",{"error":"Invalid User"})
+    if request.method == "POST":
+        try:
+            admin = AdminLoginModel.objects.get(username=request.POST.get("pwn_username"),
+                                                password=request.POST.get("pwn_password"))
+            request.session["admin_status"] = True
+            return redirect('welcome')
+        except:
+            return render(request, "pwn/login.html", {"error": "Invalid User"})
+    else:
+        request.session["admin_status"] = False
+        return render(request, "pwn/login.html", {"error": "Admin Logout Success"})
+
 
 
 def welcome(request):
-    return render(request,"pwn/welcome.html")
+    return render(request,"pwn/home.html")
+
+
+def openState(request):
+    return render(request,"pwn/openstate.html")
+
+
+def openCity(request):
+    return render(request,"pwn/opencity.html")
+
+
+def openCusine(request):
+    return render(request,"pwn/opencuisine.html")
+
+
+def openVendor(request):
+    return render(request,"pwn/openvendor.html")
+
+
+def openReporsts(request):
+    return render(request,"pwn/openreports.html")
